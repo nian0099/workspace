3152d433fcbfbe4ad1f1afc3bc4c74bb9239a053
nian0099 <nian0099@163.com>
2018-03-08 18:38:29 +0800

    初始参数化excel用例

--- Test_framework/src/utils/file_reader.py
+++ Test_framework/src/utils/file_reader.py
@@ -2,6 +2,7 @@
 
 import yaml
 import os
+from xlrd import open_workbook
 
 
 class YamlReader:
@@ -19,3 +20,36 @@ class YamlReader:
             with open(self.yamlf, 'rb') as f:
                 self._data = list(yaml.safe_load_all(f))  # load后是个generator，用list组织成列表
         return self._data
+
+class SheetTypeError(Exception):
+    pass
+
+class ExcelReader:
+    """
+        读取excel文件中的内容。返回list。
+
+        如：
+        excel中内容为：
+        | A  | B  | C  |
+        | A1 | B1 | C1 |
+        | A2 | B2 | C2 |
+
+        如果 print(ExcelReader(excel, title_line=True).data)，输出结果：
+        [{A: A1, B: B1, C:C1}, {A:A2, B:B2, C:C2}]
+
+        如果 print(ExcelReader(excel, title_line=False).data)，输出结果：
+        [[A,B,C], [A1,B1,C1], [A2,B2,C2]]
+
+        可以指定sheet，通过index或者name：
+        ExcelReader(excel, sheet=2)
+        ExcelReader(excel, sheet='BaiDuTest')
+    """
+    def __init__(self,excel,sheet = 0,title_line = True):
+        if os.path.exists(excel):
+            self.excle = excel
+        else:
+            raise FileNotFoundError("excel文件不存在!!!")
+        self.sheet = sheet
+        self.title_line = title_line
+        self._data = list()
+
